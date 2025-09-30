f1d6c291d2a67d0b8956fc45294be688ce1b6b32
teste <teste@gmail.com>
2025-09-30 20:28:24 -0300

    backend cofig

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AlimentoController;
-
-Route::apiResource('alimentos', AlimentoController::class);
